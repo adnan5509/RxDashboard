fcf23f7ba3b6cc53dc729e446c4285fd7e43f62e
adnan5509 <adnanafzal5509@gmail.com>
2025-04-16 20:18:49 +0200

    implement OnInit lifecycle hook in AppComponent to log interval values and manage subscription cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private destroyRef = inject(DestroyRef);
+  ngOnInit() {
+    const intervalSubscription = interval(5000).subscribe({
+      next: (val) => { console.log(val) }
+    })
+
+    this.destroyRef.onDestroy(() => {
+      intervalSubscription.unsubscribe();
+    });
+  }
 
 }

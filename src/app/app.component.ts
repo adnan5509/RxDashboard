b4ee54f0f1d67dc3fd803400e5e0b30ac1f85619
adnan5509 <adnanafzal5509@gmail.com>
2025-04-17 17:22:30 +0200

    add custom observable for interval tracking and update result messaging in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { filter, interval, map, max, takeWhile } from 'rxjs';
+import { interval, takeWhile, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,44 +8,71 @@ import { filter, interval, map, max, takeWhile } from 'rxjs';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-
   clickCounter = signal(0);
   clickCounter$ = toObservable(this.clickCounter);
-  $interval = interval(150).pipe(takeWhile((val) => { return val <= 100 }));
-  intervalSignal = toSignal(this.$interval, { initialValue: 0 });
+
+  interval$ = interval(200).pipe(takeWhile((val) => val <= 50));
+  intervalSignal = toSignal(this.interval$, { initialValue: 0 });
+
+  userWinning = false;
+  currentResultMessage = signal('Click the button to start counting...');
   fade = true;
 
-  constructor() {
-    // effect(() => {
-    //   console.log('Click count:', this.clickCounter());
-    // })
-  }
-
   private destroyRef = inject(DestroyRef);
-  ngOnInit() {
-    // const intervalSubscription = interval(1000).pipe(
-    //   map((val) => { return val * 3 }),
-    //   filter((val) => { return val % 2 === 0 })
-    // ).subscribe({
-    //   next: (val) => { console.log(val) }
-    // })
 
+  //  Custom Observable using `new Observable`
+  customInterval$ = new Observable<boolean>((subscriber) => {
+    const intervalId = setInterval(() => {
+      const intervalVal = this.intervalSignal();
+      const clickVal = this.clickCounter();
+
+      if (intervalVal >= 50) {
+        subscriber.complete(); // end observable
+        clearInterval(intervalId);
+      }
+
+      if (clickVal > intervalVal) {
+        subscriber.next(true);
+      } else {
+        subscriber.next(false);
+      }
+    }, 200);
+  });
+
+  ngOnInit() {
+    //  React to emitted values from the custom observable
+    const customIntervalSubscription = this.customInterval$.subscribe({
+      next: (val) => {
+        this.userWinning = val;
+        this.currentResultMessage.set(val ? 'You are winning!' : 'You are losing! Fast click!');
+      },
+      complete: () => {
+        this.currentResultMessage.set(this.userWinning ? 'You won!' : 'You lost!');
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      customIntervalSubscription.unsubscribe();
+    });
+
+    // Optional click logging
     const clickCounterSubscription = this.clickCounter$.subscribe({
-      next: (val) => console.log('Click count:', this.clickCounter()),
+      next: () => console.log('Click count:', this.clickCounter())
     });
 
     this.destroyRef.onDestroy(() => {
       clickCounterSubscription.unsubscribe();
     });
-
   }
 
   onClick() {
-    this.fade = false; // reset animation
-    this.clickCounter.update(val => val + 1);
+    this.fade = false;
+    if (this.intervalSignal() <= 50 && this.clickCounter() <= 50) {
+      this.clickCounter.update(val => val + 1);
+    }
 
     setTimeout(() => {
       this.fade = true;
-    }, 50); // brief timeout to re-trigger animation
+    }, 50);
   }
 }
